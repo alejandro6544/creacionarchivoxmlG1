942f25cd8e3d7bdf8ef85f50e2a0cda6929ad3c5
dacastro <dacastro@SCG-21V-D58724.uao.edu.co>
2018-08-14 12:17:31 -0500

    Se crea el método para guardar archivos XML además se modifica la interfaz gráfica y la clase estudiante

--- src/creacionarchivoxmlg1/FXMLDocumentController.java
+++ src/creacionarchivoxmlg1/FXMLDocumentController.java
@@ -6,30 +6,67 @@
 package creacionarchivoxmlg1;
 
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import modelo.Estudiante;
 
 /**
  *
  * @author dacastro
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private Label lnombre;
+    @FXML
+    private Label ltitulo;
+    @FXML
+    private Label ltelefono;
+    @FXML
+    private Label lcorreo;
+    @FXML
+    private Label lcodigo;
+    @FXML
+    private Label lcarrera;
+    @FXML
+    private TextField tnombre;
+    @FXML
+    private TextField ttelefono;
+    @FXML
+    private TextField tcorreo;
+    @FXML
+    private TextField tcodigo;
+    @FXML
+    private TextField tcarrera;
+
+    LinkedList<Estudiante> listaE;
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+        String nombre = tnombre.getText();
+        String telefono = ttelefono.getText();
+        String correo = tcorreo.getText();
+        String codigo = tcodigo.getText();
+        String carrera = tcarrera.getText();
+
+        Estudiante objE = new Estudiante(codigo, carrera, nombre, telefono, correo);
+        listaE.add(objE);
     }
-    
+
+    @FXML
+    private void guardarXML(ActionEvent event) {
+        Estudiante objeV = new Estudiante();
+        boolean guardar = objeV.crearArchivoXML(listaE);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        listaE = new LinkedList<>();
+    }
+
 }
